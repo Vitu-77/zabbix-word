c28985e701cb3bfae52a60ee53c2544ecc4702d9
Victor Rodrigues <marcelo.victor05@gmail.com>
2020-05-15 19:18:59 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+
+module.exports = app;
