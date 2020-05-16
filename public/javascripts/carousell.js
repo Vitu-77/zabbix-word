e13a1a6ece8747c9ae54c55f65c644ba27c1533f
Victor Rodrigues <marcelo.victor05@gmail.com>
2020-05-16 14:20:10 -0300

    add: carousell animations

--- public/javascripts/carousell.js
+++ public/javascripts/carousell.js
@@ -1,24 +1,124 @@
-import { slides, max, slideTimeout } from './constants.js';
+import { SLIDES, MAX, PRESENTATION_TIMEOUT } from './constants.js';
+
+const pausePresentation = false;
+
+// img-container
 
 let carousellIndex = 1;
 const nextButton = document.querySelector('#next');
 const backButton = document.querySelector('#back');
 
-const handleNextSlide = () => {
-    if (carousellIndex === max) {
-        return carousellIndex = 1;
-    }
+const slidesIndicatorsContainer = document.querySelector('.carrousell-index-container');
+const slidesIndicators = document.querySelectorAll('.carrousell-index');
 
-    return carousellIndex = carousellIndex + 1;
+const home = document.querySelector('section.home');
+const coverContainer = document.querySelector('#site-cover-container');
+const cover = document.querySelector('#site-cover');
+const descriptionTitle = document.querySelector('#description-title');
+const descriptionParagraph = document.querySelector('#description-paragraph');
+const seeMore = document.querySelector('#see-more');
+const descriptionOuterContainer = document.querySelector('.description');
+const descriptionInnerContainer = document.querySelector('#decription-container');
+
+const clearActiveClass = () => slidesIndicators.forEach(indicator => indicator.classList.remove('active'));
+const activeIndicator = () => slidesIndicators[carousellIndex - 1].classList.add('active');
+
+const setSeeMorePosition = () => {
+    const descriptionInnerContainerHeight = descriptionInnerContainer.offsetHeight;
+    const descriptionOuterContainerHeight = descriptionOuterContainer.offsetHeight;
+    const seeMoreHeight = seeMore.offsetHeight;
+
+    const position = descriptionOuterContainerHeight - descriptionInnerContainerHeight - seeMoreHeight;
+    seeMore.style.transform = `translateY(-${position}px)`;
 }
 
-const handlePrevSlide = () => {
+const alterSlide = () => {
+    const { description, title, img, ancor, homeBg } = SLIDES[carousellIndex - 1];
+
+    const animate = (element, attribute, css) => {
+        element.style[css.property] = css.initialValue;
+
+        setTimeout(() => element[attribute.attr] = attribute.value, 200);
+        setTimeout(() => {
+            element.style[css.property] = css.finalValue;
+            home.style.background = homeBg;
+        }, 200);
+        setTimeout(() => setSeeMorePosition(), 201);
+    }
+
+    animate(
+        descriptionTitle,
+        { attr: 'innerHTML', value: title },
+        { property: 'opacity', initialValue: 0, finalValue: 1 },
+    );
+
+    animate(
+        descriptionParagraph,
+        { attr: 'innerHTML', value: description },
+        { property: 'opacity', initialValue: 0, finalValue: 1 },
+    );
+
+    animate(
+        coverContainer,
+        {},
+        { property: 'opacity', initialValue: 0, finalValue: 1 },
+    );
+
+    animate(
+        cover,
+        { attr: 'src', value: img },
+        { property: 'opacity', initialValue: 0, finalValue: 1 },
+    );
+
+    seeMore.href = ancor;
+}
+
+const nextSlide = () => {
+    clearActiveClass(slidesIndicators);
+
+    if (carousellIndex === MAX) {
+        carousellIndex = 1;
+        activeIndicator(slidesIndicators);
+        return alterSlide();
+    }
+
+    carousellIndex = carousellIndex + 1;
+    activeIndicator(slidesIndicators);
+    return alterSlide();
+}
+
+const prevSlide = () => {
+    clearActiveClass(slidesIndicators);
+
     if (carousellIndex === 1) {
-        return carousellIndex = max;
+        carousellIndex = MAX;
+        activeIndicator(slidesIndicators);
+        return alterSlide();
     }
 
-    return carousellIndex = carousellIndex - 1;
+    carousellIndex = carousellIndex - 1;
+    activeIndicator(slidesIndicators);
+    return alterSlide();
 }
 
-nextButton.addEventListener('click', handleNextSlide);
-backButton.addEventListener('click', handlePrevSlide);
+window.onload = () => {
+    setSeeMorePosition();
+
+    slidesIndicators.forEach((indicator) => {
+        return indicator.addEventListener('click', (e) => {
+            clearActiveClass();
+
+            carousellIndex = Number(e.target.attributes.index.value);
+
+            activeIndicator(slidesIndicators);
+            return alterSlide();
+        });
+    });
+
+    nextButton.addEventListener('click', nextSlide);
+    backButton.addEventListener('click', prevSlide);
+
+    setInterval(() => {
+        nextSlide();
+    }, PRESENTATION_TIMEOUT);
+}
--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -1,17 +1,28 @@
-const slides = [
+const SLIDES = [
     {
         title: 'Zabbix reports',
-        img: '/images/zabbix-reports-banner.png',
+        img: '/images/zabbix-reports-banner-small.png',
+        ancor: 'http://zabbix.reports.com',
+        homeBg: 'linear-gradient(292.62deg, #f57b51 -8.19%, #d63447 96.8%)',
         description: 'Com o Zabbix Reports, você pode gerar relatórios a partir dos seus Zabbixes e customizá-los como quiser',
     },
     {
         title: 'Data summary',
         img: '/images/zabbix-reports-banner.png',
+        ancor: 'http://summary.data.com',
+        homeBg: 'linear-gradient(296.14deg, #f57b51 27.7%, #d63447 130.31%)',
         description: ' O Zabbix é uma soluçao de monitoraçao de ativos, que vem sendo utilizada em grande escala por milhares de empresas, dentre suas varias funcionalidade ele te entrega uma API para buscar dados e automatizar tarefas, mas se voce não sabe programar fica inviavel utilizar da melhor maneira, o Data Summary nasceu como uma soluçao para abstrair essa dificuldade e entregar ao usuario uma forma mais facil e agil de extrair esses dados.',
+    },
+    {
+        title: 'Teste GIF',
+        img: '/images/gif-test.gif',
+        ancor: 'http://zabbix.reports.com',
+        homeBg: 'linear-gradient(292.62deg, #f57b51 -8.19%, #d63447 96.8%)',
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, omnis in! Voluptate, accusantium excepturi veritatis deleniti totam inventore minima! Vel!',
     },
 ];
 
-const max = 2;
-const slideTimeout = 5000;
+const MAX = SLIDES.length;
+const PRESENTATION_TIMEOUT = 5000;
 
-export { slides, max, slideTimeout }
+export { SLIDES, MAX, PRESENTATION_TIMEOUT }
